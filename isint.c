fe99dd86d64a0d7ce598542850ad219d5c88405e
Lord-Gusarov <2367@holbertonschool.com>
2020-12-19 15:58:17 -0400

    Defining the function that check if a string is made only of decimal digits

--- isint.c
+++ isint.c
@@ -1,6 +1,20 @@
 #include "monty.h"
 
+/**
+ * isint - determine if a string is composed only
+ * of decimal digits
+ * @str: string to check
+ * Return: 1 if just digits, 0 otherwise
+ */
 int isint(char *str)
 {
-	return 1;
+	if (str == NULL)
+		return (0);
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
 }
