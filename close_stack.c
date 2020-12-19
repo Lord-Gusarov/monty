e7cebc1de7c40cdedaf176f5ddeaf75adcdec2e9
Jeff Martinez <2408@holbertonschool.com>
2020-12-19 16:59:34 +0000

    Adding a function to close and exit.

---
+++ close_stack.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * error - prints error message.
+ * @str: string message.
+ * Return: nothing.
+ */
+void close_stack(stack_t *stack, int status)
+{
+	
+	exit(status);
+}
