3b344ed03f9083df8c6834d65baaa6555b036e3a
Lord-Gusarov <2367@holbertonschool.com>
2020-12-19 16:01:31 -0400

    blanck function, just declaration

---
+++ swap.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: stack from which to swap
+ * @line_number: ByteCode line
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+
+
+}
