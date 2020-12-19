211fd150dd547fd264c3fac1d05a519b7e070a96
Jeff Martinez <2408@holbertonschool.com>
2020-12-19 22:34:57 +0000

    Adding a function that adds the top two elements of the stack.

--- add.c
+++ add.c
@@ -7,5 +7,24 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
+	stack_t *temp = (*stack)->prev;
 
+	if (*stack == NULL || (*stack)->prev == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		close_stack(*stack, EXIT_FAILURE);
+	}
+	(*stack)->n += temp->n;
+	if (temp->prev == NULL)
+	{
+		free(temp);
+		(*stack)->prev = NULL;
+	}
+	else
+	{
+		temp->prev->next = temp->next;
+		(*stack)->prev = temp->prev;
+		free(temp);
+	}
+	return (0);
 }
