9588563ee34e8b4e5c3e02d064f1f3f603ea2d94
Jeff Martinez <2408@holbertonschool.com>
2020-12-19 22:32:59 +0000

    Adding a function that removes the top element of the stack.

--- pop.c
+++ pop.c
@@ -7,4 +7,15 @@
  **/
 void pop(stack_t **stack, unsigned int line)
 {
+	stack_t *temp = (*stack);
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line);
+		close_stack(*stack, EXIT_FAILURE)
+	}
+	temp->prev->next = NULL;
+	(*stack) = temp->prev;
+	free(temp);
+	return (0);
 }
