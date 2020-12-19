e0a15b896e838d1a31f471858d1e5e2d213ecd8f
Jeff Martinez <2408@holbertonschool.com>
2020-12-19 17:00:59 +0000

    Adding a function that adds new elements to the stack.

---
+++ push.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * push - adds an element to the stack.
+ * @tok: element to be added.
+ * Return: nothing.
+ */
+void push(stack_t **stack, int l_cnt, char *tok)
+{
+	stack_t *new;
+
+	if (isint(tok) == 0)
+		close_stack(stack, EXIT_FAILURE);
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		fprint(stderr, "Error: malloc failed");
+		close_stack(stack, EXIT_FAILURE);
+	}
+	new->n = atoi(tok)
+	new->next = NULL;
+	if (!stack)
+	{
+		new->prev = NULL;
+		*stack = new;
+		return;
+	}
+	new->prev = *stack;
+	(*stack)->next = new;
+	*stack = new;
+	return;
+}
