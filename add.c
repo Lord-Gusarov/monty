72ba776d97bfeee88c44c976317507189b379dc1
Lord-Gusarov <2367@holbertonschool.com>
2020-12-19 16:00:53 -0400

    blank function

---
+++ add.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+/**
+ * add - adds the top two elements of the stack
+ * @stack: stack from which to add
+ * @line_number: ByteCode line number
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+
+}
