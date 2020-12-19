f639fbffd4f0953a4f3bdfe2a056e449032a771a
Lord-Gusarov <2367@holbertonschool.com>
2020-12-19 18:40:15 -0400

    fully implemented, now with the global var to free and close everything as needed

--- close_stack.c
+++ close_stack.c
@@ -1,11 +1,19 @@
 #include "monty.h"
 
 /**
- * error - prints error message.
+ * close_stack - Makes surs the build of the interpreter gets
+ * closed/free, frees the node in the stacks and set the
+ * appropiate exit status..
  * @str: string message.
  * Return: nothing.
  */
 void close_stack(stack_t *stack, int status)
 {
+	if(b.fd)
+		close(b.fd);
+	if (b.tok)
+		freeStrArr(b.tok);
+	if(stack)
+		free_stack(stack);
 	exit(status);
 }
