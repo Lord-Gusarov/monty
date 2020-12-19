9993832322e40bbdd19bb6934040fb277937a732
Jeff Martinez <2408@holbertonschool.com>
2020-12-19 22:31:21 +0000

    Adding a function that prints the value at the top of the stack, followed by a new line.

--- pint.c
+++ pint.c
@@ -7,4 +7,10 @@
  **/
 void pint(stack_t **stack, unsigned int line)
 {
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line);
+		close_stack(*stack, EXIT_FAILURE);
+	}
+	print("%d\n", (*stack)->n);
 }
