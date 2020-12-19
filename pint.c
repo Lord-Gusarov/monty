23b94a74c4ace041c28135f9a753a65cef2e8db6
Jeff Martinez <2408@holbertonschool.com>
2020-12-19 19:55:40 +0000

    Adding empty functions for later.

---
+++ pint.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+/**
+ * pint - prints the value at the top of the stack, followed by a new line.
+ * @stack: pointer to the stack.
+ * @line: line number.
+ * Return: nothing.
+ **/
+void pint(stack_t **stack, unsigned int line)
+{
+}
---
+++ pop.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+/**
+ * pop - removes the top element of the stack.
+ * @stack: pointer to the stack.
+ * @line: line number.
+ * Return: nothing.
+ **/
+void pop(stack_t **stack, unsigned int line)
+{
+}
