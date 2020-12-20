85fd92f21ece3702d244f02a4931d92ff0d138e6
Jeff Martinez <2408@holbertonschool.com>
2020-12-20 16:06:35 +0000

    Adding a function that  rotates the stack to the top.

---
+++ rotr.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * rotr - rotates the stack to the bottom
+ * @stack: pointer to stack
+ * @line_number: ByteCode line
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = (*stack), *temp2;
+	(void)line_number;
+
+	while (temp->prev != NULL)
+	{
+		temp = temp->prev;
+	}
+	temp2 = temp->next;
+	temp->next = NULL;
+	temp->prev = *stack;
+	(*stack)->next = temp;
+	temp2->prev = NULL;
+	*stack = temp;
+}
