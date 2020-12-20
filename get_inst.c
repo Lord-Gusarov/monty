b5026dbfd0ce97ffc33594f6cc6c205b9b9d5e77
Lord-Gusarov <2367@holbertonschool.com>
2020-12-20 08:58:17 -0400

    function to handle ByteCode nop declared in new file and added to get_inst()

--- get_inst.c
+++ get_inst.c
@@ -7,11 +7,13 @@
 void (*get_inst(char *tok))(stack_t **stack, unsigned int line)
 {
 	instruction_t arr[] = {
+		{"push", push},
 		{"pall", pall},
 		{"pint", pint},
 		{"pop", pop},
 		{"swap", swap},
 		{"add", add},
+		{"nop", nop},
 		{NULL, NULL},
 	};
 	int i = 0;
---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @stack: front of the stack
+ * @line: ByteCode Line number
+ */
+void nop(stack_t **stack, unsigned int line)
+{
+	(void)(stack);
+	(void)(line);
+}
