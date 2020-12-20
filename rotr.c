06969ff63377d6e714c9fea431d59099cab70282
Jeff Martinez <2408@holbertonschool.com>
2020-12-20 16:09:21 +0000

    Adding a function that rotates the stack to the bottom.

--- rotr.c
+++ rotr.c
@@ -10,14 +10,17 @@ void rotr(stack_t **stack, unsigned int line_number)
 	stack_t *temp = (*stack), *temp2;
 	(void)line_number;
 
-	while (temp->prev != NULL)
+	if (*stack && (*stack)->prev)
 	{
-		temp = temp->prev;
+		while (temp->prev != NULL)
+		{
+			temp = temp->prev;
+		}
+		temp2 = temp->next;
+		temp->next = NULL;
+		temp->prev = *stack;
+		(*stack)->next = temp;
+		temp2->prev = NULL;
+		*stack = temp;
 	}
-	temp2 = temp->next;
-	temp->next = NULL;
-	temp->prev = *stack;
-	(*stack)->next = temp;
-	temp2->prev = NULL;
-	*stack = temp;
 }
