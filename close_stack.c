235621a8420a8f1650fb6ab7bdb532190a95e115
Lord-Gusarov <2367@holbertonschool.com>
2020-12-20 09:00:40 -0400

    function declaration changed, and now it SAFELY frees everything from the global var

--- close_stack.c
+++ close_stack.c
@@ -3,17 +3,17 @@
 /**
  * close_stack - Makes surs the build of the interpreter gets
  * closed/free, frees the node in the stacks and set the
- * appropiate exit status..
- * @str: string message.
- * Return: nothing.
+ * appropiate exit status
+ * @stack: front of the stack
+ * @status: desired exit status
  */
-void close_stack(stack_t *stack, int status)
+void close_stack(int status)
 {
-	if(b.fd)
-		close(b.fd);
+	if (b.fd)
+		fclose(b.fd);
+	sfree(&b.buf);
 	if (b.tok)
 		freeStrArr(b.tok);
-	if(stack)
-		free_stack(stack);
+	free_stack(b.stack);
 	exit(status);
 }
