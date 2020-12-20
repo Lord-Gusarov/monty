48977a6c33c0c2df394a692e6f3e84db21aa5226
Jeff Martinez <2408@holbertonschool.com>
2020-12-20 14:27:11 +0000

    Fixed code for seg fault.

--- add.c
+++ add.c
@@ -7,24 +7,14 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = (*stack)->prev;
+	stack_t *temp;
 
 	if (*stack == NULL || (*stack)->prev == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		close_stack(EXIT_FAILURE);
 	}
-	(*stack)->n += temp->n;
-	if (temp->prev == NULL)
-	{
-		free(temp);
-		(*stack)->prev = NULL;
-	}
-	else
-	{
-		temp->prev->next = temp->next;
-		(*stack)->prev = temp->prev;
-		free(temp);
-	}
-	return;
+	temp = (*stack)->prev;
+	temp->n += (*stack)->n;
+	pop(stack, line_number);
 }
